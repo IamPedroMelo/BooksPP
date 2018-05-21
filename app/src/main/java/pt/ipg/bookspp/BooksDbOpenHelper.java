922c7b0e35910f26140a12321bbb88684381e9ea
jei <pedromelop@hotmail.com>
2018-05-21 11:26:28 +0100

    Dbasehelper

---
+++ app/src/main/java/pt/ipg/bookspp/BooksDbOpenHelper.java
@@ -0,0 +1,25 @@
+package pt.ipg.bookspp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class BooksDbOpenHelper extends SQLiteOpenHelper {
+
+    private static final int VERSION = 1;
+    private static final String DATABASE_NAME = "books.db";
+
+    public BooksDbOpenHelper(Context context, SQLiteDatabase.CursorFactory factory) {
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
